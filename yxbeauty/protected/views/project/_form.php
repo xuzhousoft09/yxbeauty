2c95f1a38a0746da0de17f6bad924fa94c4dca13
qiang.lv <xzsdeveloper@gmail.com>
2019-07-02 17:27:38 +0800

    commit

--- yxbeauty/protected/views/project/_form.php
+++ yxbeauty/protected/views/project/_form.php
@@ -6,9 +6,8 @@
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<?php echo $form->errorSummary($model); ?>
-
+	
+    	<?php var_dump($model); exit;?>>
 	<div class="row">
 		<?php echo $form->labelEx($model,'姓名'); ?>
 		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>128)); ?>
